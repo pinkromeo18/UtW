e7b6c41b24240e45df6d62bcb392cab70341eca7
ピンクロメオ <99705203+pinkromeo18@users.noreply.github.com>
2022-04-02 21:59:09 +0900

    Create key.js

---
+++ key.js
@@ -0,0 +1,61 @@
+
+var _nowkey={
+    '_U':'w',
+    '_L':'a',
+    '_D':'s',
+    '_R':'d',
+    
+    'A':'z',
+    'B':'c',
+    'X':'x',
+    'Y':'v',
+
+    'L':'q',
+    'R':'e',
+    
+    'S':'Enter',
+    'P':' '
+}
+
+var nowkey=void 0;
+
+export function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export function keyconfig(obj){
+  if(!obj) nowkey=Object.assign({},_nowkey);
+  nowkey = Object.assign({},nowkey,obj);
+  console.log('keyconfig')
+  console.log(JSON.stringify(nowkey,null,2))
+  return;
+}
+
+export function key(){return new Promise(sol=>{
+  if(!nowkey) keyconfig();
+  var dde =document.documentElement;
+  dde.onkeydown = (ev)=>{
+    var one = Object.keys(nowkey)
+     .filter(d=>nowkey[d]===ev.key).join('')
+    if(one)return dde.onkeydown = null,sol(one)
+  }
+})
+}
+
+export async function keyloop(caller){
+    await caller(await key(),sleep)
+    return keyloop(caller)
+}
+
+/*
+import {keyloop,key,sleep} from "https://pinkromeo18.github.io/UtW/key.js";
+var debug=(str)=>document.querySelector('pre').textContent=str;
+
+keyloop(async(k,wait)=>{
+  
+  debug(k)
+  await wait(1000)
+  debug('next');
+  
+})
+*/
